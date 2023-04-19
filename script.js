19c78bd16676f0c9eb457b6a6c907fae2e47b1ce
Justin Sanchez <justinlsanchez12@gmail.com>
2023-04-18 22:37:09 -0700

    adding local storage and save button function

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
+var eventsData;
+
 function setHourColors(){
     var now = dayjs();
 
     for (var i = 9; i < 18; i++) {
+        $('#hour-' + i + " textarea").removeClass("future");
         if (i < now.hour()){
-            $("#hour" + i + " textarea").addClass("past");
+            $("#hour-" + i + " textarea").addClass("past");
         }
         else if (i == now.hour()) {
-            $("hour" + i + " textarea").addClass("present");
+            $("hour-" + i + " textarea").addClass("present");
         }
     }
 }
 
+function loadStoredData() {
+    eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
+    if(!eventsData){
+        eventsData = {
+            hour9: "",
+            hour10: "",
+            hour11: "",
+            //ETC
+        };
+    }
+}
+
+function handleSaveClick(event) {
+    // grab data from HTML
+    var hourBlock = $(event.target).parent();
+    var value = hourBlock.parent().children("textarea").val();
+    var hour = hourBlock.attr('id').split("-")[1];
+
+    //modify our data object
+    eventsData["hour" + hour] = value;
+
+    // store in local storage
+    localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
+}
+
 $(function(){
+    loadStoredData();
     setHourColors();
 });
+
+$('.saveBtn').on('click', handleSaveClick);
